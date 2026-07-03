0e58e0b00e65eab9b676193a47cfc4c8b5cdae6e
ORIN-AYO <orinayo2022@gmail.com>
2026-07-03 19:10:17 +0100

    watermark content position fixed

--- components/home/RadialShowcase.tsx
+++ components/home/RadialShowcase.tsx
@@ -48,7 +48,7 @@ export default function RadialShowcase() {
         {/* Cascading Watermarks */}
         <div
           className="absolute w-full flex flex-col items-center pointer-events-none select-none z-0 font-[bebas_neue]"
-          style={{ bottom: "245px", left: 0 }}
+          style={{ bottom: "137px", left: 0 }}
           aria-hidden
         >
           <div style={{ fontSize: "170px", color: "transparent", WebkitTextStroke: "1px #000000", opacity: 0.2, lineHeight: 1, }}>UPWEALTH</div>
@@ -57,66 +57,66 @@ export default function RadialShowcase() {
         </div>
 
         <div className="max-w-6xl mx-auto">
-        <div className="relative flex items-center justify-center" style={{ height: 600 }}>
-          <motion.div
-            initial={{ opacity: 0, scale: 0.9 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5 }}
-            viewport={{ once: true }}
-            className="relative z-20 w-72"
-          >
-            <div className="aspect-[3/4] relative" style={{ top: (5), left: '50%', transform: 'translate(-50%, -50%)' }}>
-              <img
-                src="/images/radial-1.png"
-                alt={FEATURED_ISSUE.coverAlt}
-                className="absolute  object-cover"
-              />
-            </div>
+          <div className="relative flex items-center justify-center" style={{ height: 600 }}>
+            <motion.div
+              initial={{ opacity: 0, scale: 0.9 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5 }}
+              viewport={{ once: true }}
+              className="relative z-20 w-72"
+            >
+              <div className="aspect-[3/4] relative" style={{ top: (5), left: '50%', transform: 'translate(-50%, -50%)' }}>
+                <img
+                  src="/images/radial-1.png"
+                  alt={FEATURED_ISSUE.coverAlt}
+                  className="absolute  object-cover"
+                />
+              </div>
 
-            <div style={{ position: 'absolute', left: '0.92%', right: '-5%', top: '-41%', bottom: '43.9%', background: '#F0F0F3', zIndex: -1 }}>
-              <svg width="395" height="512" viewBox="0 0 395 512" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full h-full" preserveAspectRatio="none">
-                <rect width="395" height="512" fill="#F0F0F3" />
-              </svg>
-            </div>
-          </motion.div>
+              <div style={{ position: 'absolute', left: '0.92%', right: '-5%', top: '-41%', bottom: '43.9%', background: '#F0F0F3', zIndex: -1 }}>
+                <svg width="395" height="512" viewBox="0 0 395 512" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full h-full" preserveAspectRatio="none">
+                  <rect width="395" height="512" fill="#F0F0F3" />
+                </svg>
+              </div>
+            </motion.div>
 
-          {/* Carousel 2 */}
-          <div className="absolute z-10" style={{ left: -32, top: 208 }}>
-            <div className="w-28 magazine-cover" style={{ transform: "rotate(8.15deg)" }}>
-              <div className="aspect-[3/4] bg-[#EBEBED] relative overflow-hidden shadow-cover">
-                <img src="/images/carousel-2.png" alt="Carousel 2" className="w-full h-full object-cover" />
+            {/* Carousel 2 */}
+            <div className="absolute z-10" style={{ left: -32, top: 208 }}>
+              <div className="w-28 magazine-cover" style={{ transform: "rotate(8.15deg)" }}>
+                <div className="aspect-[3/4] bg-[#EBEBED] relative overflow-hidden shadow-cover">
+                  <img src="/images/carousel-2.png" alt="Carousel 2" className="w-full h-full object-cover" />
+                </div>
               </div>
             </div>
-          </div>
 
-          {/* Carousel 3 */}
-          <div className="absolute z-10" style={{ left: 205, top: (-47) }}>
-            <div className="w-30 magazine-cover" style={{ transform: "rotate(-11deg)" }}>
-              <div className="aspect-[3/4] bg-[#EBEBED] relative overflow-hidden shadow-cover">
-                <img src="/images/carousel-3.png" alt="Carousel 2" className="w-full h-full object-cover" />
+            {/* Carousel 3 */}
+            <div className="absolute z-10" style={{ left: 205, top: (-47) }}>
+              <div className="w-30 magazine-cover" style={{ transform: "rotate(-11deg)" }}>
+                <div className="aspect-[3/4] bg-[#EBEBED] relative overflow-hidden shadow-cover">
+                  <img src="/images/carousel-3.png" alt="Carousel 2" className="w-full h-full object-cover" />
+                </div>
               </div>
             </div>
-          </div>
 
-          {/* Carousel 4 */}
-          <div className="absolute z-10" style={{ right: 213, top: (-72) }}>
-            <div className="w-28 magazine-cover" style={{ transform: "rotate(-8.15deg)" }}>
-              <div className="aspect-[3/4] bg-[#EBEBED] relative overflow-hidden shadow-cover">
-                <img src="/images/carousel-6.png" alt="Carousel 2" className="w-full h-full object-cover" />
+            {/* Carousel 4 */}
+            <div className="absolute z-10" style={{ right: 213, top: (-72) }}>
+              <div className="w-28 magazine-cover" style={{ transform: "rotate(-8.15deg)" }}>
+                <div className="aspect-[3/4] bg-[#EBEBED] relative overflow-hidden shadow-cover">
+                  <img src="/images/carousel-6.png" alt="Carousel 2" className="w-full h-full object-cover" />
+                </div>
               </div>
             </div>
-          </div>
 
-          {/* Carousel 1 */}
-          <div className="absolute z-10" style={{ right: -33, top: 212 }}>
-            <div className="w-28 magazine-cover" style={{ transform: "rotate(8.15deg)" }}>
-              <div className="aspect-[3/4] bg-[#EBEBED] relative overflow-hidden shadow-cover">
-                <img src="/images/carousel-1.png" alt="Carousel 2" className="w-full h-full object-cover" />
+            {/* Carousel 1 */}
+            <div className="absolute z-10" style={{ right: -33, top: 212 }}>
+              <div className="w-28 magazine-cover" style={{ transform: "rotate(8.15deg)" }}>
+                <div className="aspect-[3/4] bg-[#EBEBED] relative overflow-hidden shadow-cover">
+                  <img src="/images/carousel-1.png" alt="Carousel 2" className="w-full h-full object-cover" />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
       </div>
     </section>
   );
